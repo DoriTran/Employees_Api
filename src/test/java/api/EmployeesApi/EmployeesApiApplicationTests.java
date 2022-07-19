83e11fb33ed6a3a47642275fd7b442c2d74b1698
DoriTran <tranquocdong308@gmail.com>
2022-07-19 09:19:18 +0700

    first commit

---
+++ src/test/java/api/EmployeesApi/EmployeesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.EmployeesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
